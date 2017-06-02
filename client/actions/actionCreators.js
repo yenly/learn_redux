3e7dd3f4ec8537b344cb0053e60876109dfadb84
Yenly Ma <yencodes@gmail.com>
2017-06-01 18:47:53 -0700

    Create redux actions in actionCreator file.

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,25 @@
+// increment likes
+export increment(index) {
+  return {
+    type: 'INCREMENT_LIKES',
+    index
+  }
+}
+
+// add comment
+export addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
+
+// remove comment
+export removeComment(postId, i) {
+  return {
+    type: 'REMOVE_COMMENT',
+    i
+  }
+}
