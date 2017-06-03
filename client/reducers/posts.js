a7614657df0052096fc51c19afcc1e2230665563
Yenly Ma <yencodes@gmail.com>
2017-06-03 15:17:09 -0700

    Make photo reducer update likes in state.

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,10 +3,25 @@
 // 1. the action (info about what happened)
 // 2. copy of current state
 
+// redux uses functional programming
+// has to use pure functions
+
 function posts(state = [], action) {
-  console.log("The post will change.");
-  console.log(state, action);
-  return state;
+  // console.log("The post will change.");
+  // console.log(state, action);
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+      // return the updated state
+      // console.log("incrementing likes..")
+      const i = action.index;
+      return [
+        ...state.slice(0,i), // before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1), // after the one we are updating
+      ]
+    default:
+      return state;
+  }
 }
 
 export default posts;
