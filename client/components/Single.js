4afe296aee05aa4e71e7f49496b1b44e7459ddfb
Yenly Ma <yencodes@gmail.com>
2017-06-04 16:14:56 -0700

    Add display single photo component and placeholder comments component.

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    // index of the post
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+
+    // get us the post
+    const post = this.props.posts[i];
+    //console.log(post);
+
     return (
       <div className="single-photo">
-        I'm single photo.
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
       </div>
     )
   }
